d2ce72f624c57bfb006a83689b57d9eae60b9e4b
Raul Moscoso <raul.moscoso84@gmail.com>
2021-01-19 08:18:05 -0600

    Avance2 webplayground

--- webplayground/registration/views.py
+++ webplayground/registration/views.py
@@ -17,7 +17,7 @@ class SignUpView(CreateView):
 		form = super(SignUpView, self).get_form()
 		#Modificar en tiempo real
 		form.fields['username'].widget = forms.TextInput(attrs={'class':'form-control mb-5', 'placeholder':'Nombre de Usuario'})
-		form.fields['email'].widget = forms.EmailInput(attrs={'class':'form-control mb-5', 'placeholder':'Eamail'})
+		form.fields['email'].widget = forms.EmailInput(attrs={'class':'form-control mb-5', 'placeholder':'Email'})
 		form.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control mb-5', 'placeholder':'contraseña'})
 		form.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control mb-5', 'placeholder':'Repite la contrasena'})
 		return form	
--- webplayground/webplayground/settings.py
+++ webplayground/webplayground/settings.py
@@ -126,3 +126,11 @@ STATIC_URL = '/static/'
 #Auth redirects
 LOGIN_REDIRECT_URL = 'pages:pages'
 LOGOUT_REDIRECT_URL = 'home'
+
+#Emails
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+    EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_mails')
+else:
+    #Aqui hay que configurar un email real para produccion
+    pass
