04337c45e1cba20f7c81645ab0dbd00f3cc54b96
Raul Moscoso <raul.moscoso84@gmail.com>
2021-01-20 08:59:50 -0600

    Ultimo avance

--- webplayground/messenger/views.py
+++ webplayground/messenger/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.view.generic.list import ListView
+from django.view.generic.detail import DetailView
+from.models import Thread
+
 
 # Create your views here.
+class ThreadList(ListView):
+	model: Thread
+
+	def get_queryset(self):
+
+class ThreadDetail(DetailView):
+	model: Thread
